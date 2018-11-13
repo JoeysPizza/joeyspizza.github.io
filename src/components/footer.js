5fa37d298187bb99203a67597ca5c7b217082414
Greg Jopa <gjopa@cars.com>
2018-11-13 08:53:14 -0600

    Add social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import iconFacebook from '../images/svg/facebook.svg'
+import iconYelp from '../images/svg/yelp.svg'
+import iconGoogle from '../images/svg/google.svg'
 
 const Footer = () => (
   <div className="container" style={{ margin: '20px auto' }}>
@@ -56,7 +59,41 @@ const Footer = () => (
 
       <div className="col-md">
         <h2>Links</h2>
-        <a href="https://goo.gl/maps/5HYdAwkcu8m">Joeys Pizza on Google Maps</a>
+        <div style={{ margin: '16px 0' }}>
+          <a
+            href="https://www.facebook.com/JOEYS-PIZZA-828018194256001"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={iconFacebook}
+              alt="joeys pizza on facebook"
+              style={{ width: '50px', marginRight: '20px' }}
+            />
+          </a>
+          <a
+            href="https://goo.gl/maps/5HYdAwkcu8m"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={iconGoogle}
+              alt="joeys pizza on google maps"
+              style={{ width: '50px', marginRight: '13px' }}
+            />
+          </a>
+          <a
+            href="https://www.yelp.com/biz/joey-s-pizza-hillside"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={iconYelp}
+              alt="joeys pizza on yelp"
+              style={{ width: '50px' }}
+            />
+          </a>
+        </div>
       </div>
     </div>
   </div>
