238df7f51a66ee61f8dd1e1897f7e68b51e3ac02
Greg Jopa <gjopa@cars.com>
2018-11-15 17:57:55 -0600

    Improve config for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,11 +19,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Joeys Pizza in Hillside, IL',
+        short_name: 'Joeys',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000',
+        theme_color: '#000',
         display: 'minimal-ui',
         icon: 'src/images/j-icon.png', // This path is relative to the root of the site.
       },
